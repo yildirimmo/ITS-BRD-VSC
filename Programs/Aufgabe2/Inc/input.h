7de283424946b3cd8452ec40fee1a70812314799
yildirimmo <ymadanoglu03@gmail.com>
2025-11-16 21:20:32 +0100

    zuordnung_Signal() fertiggestellt

--- Programs/Aufgabe2/Inc/input.h
+++ Programs/Aufgabe2/Inc/input.h
@@ -1,7 +1,14 @@
 #include <stdint.h>
 
+#define PHASE_A 0x00 
+#define PHASE_B 0x01 
+#define PHASE_C 0x03 
+#define PHASE_D 0x02 
+
 uint32_t zuordnung_Signal();
 
 uint32_t zuordnung_Phasenwechsel();
 
 uint32_t drehgrad_messer();
+
+uint32_t s6_leser();
--- Programs/Aufgabe2/Src/input.c
+++ Programs/Aufgabe2/Src/input.c
@@ -2,49 +2,29 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-static char PHASE_A;
-static char PHASE_B; 
-static char PHASE_C;
-static char PHASE_D;
-static uint8_t MASK_A = 0x00; //Kanal A = 0, Kanal B = 0
-static uint8_t MASK_B = 0x01; //Kanal A = 1, Kanal B = 0
-static uint8_t MASK_C = 0x03; //Kanal A = 1, Kanal B = 1
-static uint8_t MASK_D = 0x02; //Kanal A = 0, Kanal B = 1
-
-
-
+#define PHASE_A 0x00 //Kanal A = 0, Kanal B = 0
+#define PHASE_B 0x01 //Kanal A = 1, Kanal B = 0
+#define PHASE_C 0x03 //Kanal A = 1, Kanal B = 1
+#define PHASE_D 0x02 //Kanal A = 0, Kanal B = 1
 
 /**
  * @brief Hier wird das Signal aus den Kanälen (aus dem 
  * Pi Pico) gelesen und den einzelnen Phasen zugeordnet
  * 
- * Lesen aus GPIOF->IDR
+ * @return state, welches den jetzigen Zustand abbildet
  */
 uint32_t zuordnung_Signal(){
     uint8_t current = (uint8_t)GPIOF->IDR;
     uint8_t state = (current & 0x03); //um pf0 und pf1 anzugucken im idr register, der rest ist egal
-
-    //Masken für die Phasen implementieren und vergleichen
-    //Rückgabe ist einfach welche Phase gerade ist
-    PHASE_A = (MASK_A == (state & MASK_A));
-    PHASE_B = (MASK_B == (state & MASK_B));
-    PHASE_C = (MASK_C == (state & MASK_C));
-    PHASE_D = (MASK_D == (state & MASK_D));
-
-    if(PHASE_A){
-        return 1; //Returne etwas, was dem Aufrufer sagt, welche Phase gerade ist.
+    if (PHASE_A == state){
+        return PHASE_A;
+    }else if (PHASE_B == state){
+        return PHASE_B;
+    }else if (PHASE_C == state){
+        return PHASE_C;
+    }else {
+        return PHASE_D;
     }
-    if(PHASE_B){
-        return 1; //Returne etwas, was dem Aufrufer sagt, welche Phase gerade ist.
-    }
-    if(PHASE_C){
-        return 1; //Returne etwas, was dem Aufrufer sagt, welche Phase gerade ist.
-    }
-    if(PHASE_D){
-        return 1; //Returne etwas, was dem Aufrufer sagt, welche Phase gerade ist.
-    }
-    
-    return 1;
 }
 
 /**
